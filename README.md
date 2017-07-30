95cb6ebdf44fb88ca684356e5a52030dc6e448f8
Andreas Watterott <aw@watterott.net>
2017-07-30 21:38:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# VEML6070-Breakout
+Breakout with Vishay VEML6070 digital UV Light Sensor.
+
+![VEML6070-Breakout](https://github.com/watterott/VEML6070-Breakout/raw/master/hardware/VEML6070-Breakout_v11.jpg)
+
+* [Vishay VEML6070](http://www.vishay.com/docs/84277/veml6070.pdf)
+* Detects UV light
+* TWI/I2C Interface (address 0x38 and 0x39)
+* 3.3V - 5V Power Supply and Logic Level
+
+
+## Shop
+* [VEML6070-Breakout](http://www.watterott.com/en/VEML6070-Breakout-UV-light-Sensor)
+
+
+## Hardware and Software
+* [Schematics + Layout](https://github.com/watterott/VEML6070-Breakout/tree/master/hardware)
+* [Arduino Example](https://github.com/watterott/VEML6070-Breakout/tree/master/software)
+* Arduino Library and Examples: [VEML6040+VEML6070](https://github.com/embeddedadventures/MOD-1024)
---
+++ software/VEML6070.ino
@@ -0,0 +1,62 @@
+/*
+  Example sketch for VEML6070-Breakout (Digital UV Light Sensor).
+  Rset=270k on breakout, UVA sensitivity: 5.625 uW/cm²/step
+
+  Board           I2C/TWI Pins
+                  SDA, SCL
+  ----------------------------
+  Uno, Ethernet    A4, A5
+  Mega             20, 21
+  Leonardo          2,  3
+  Due              20, 21
+
+  Integration Times and UVA Sensitivity:
+    Rset=240k -> 1T=100.0ms ->   5.000 uW/cm²/step
+    Rset=270k -> 1T=112.5ms ->   5.625 uW/cm²/step
+    Rset=300k -> 1T=125.0ms ->   6.250 uW/cm²/step
+    Rset=600k -> 1T=250.0ms ->  12.500 uW/cm²/step
+*/
+
+#include <Wire.h>
+
+#define I2C_ADDR 0x38 //0x38 and 0x39
+
+//Integration Time
+#define IT_1_2 0x0 //1/2T
+#define IT_1   0x1 //1T
+#define IT_2   0x2 //2T
+#define IT_4   0x3 //4T
+
+void setup()
+{
+  Serial.begin(9600);
+  while(!Serial); //wait for serial port to connect (needed for Leonardo only)
+
+  Wire.begin();
+
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write((IT_1<<2) | 0x02);
+  Wire.endTransmission();
+  delay(500);
+}
+
+void loop()
+{
+  byte msb=0, lsb=0;
+  uint16_t uv;
+
+  Wire.requestFrom(I2C_ADDR+1, 1); //MSB
+  delay(1);
+  if(Wire.available())
+    msb = Wire.read();
+
+  Wire.requestFrom(I2C_ADDR+0, 1); //LSB
+  delay(1);
+  if(Wire.available())
+    lsb = Wire.read();
+
+  uv = (msb<<8) | lsb;
+  Serial.println(uv, DEC); //output in steps (16bit)
+
+  delay(1000);
+}
